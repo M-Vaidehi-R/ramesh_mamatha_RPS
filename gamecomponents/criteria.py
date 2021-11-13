6b16ae5c149b6f3fe013805402e69536a27d94bb
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2021-11-13 00:10:16 -0500

    did styling to the code

--- gamecomponents/criteria.py
+++ gamecomponents/criteria.py
@@ -7,29 +7,36 @@ def check(player, computer, computerlives, playerlives):
     if computer == player:
         # tie - nothing else to compare, so it'll pass
         print("tie! try again")
+        print("#_________#")
 
     elif player == "rock":
         if computer == "paper":
             print("you lose! but u got it next time!!!!")
+            print("#__________#")
             playerlives = playerlives - 1
         else:
             print("you win!   !!cheers!! :)")
+            print("#__________#")
             computerlives = computerlives - 1
 
     elif player == "paper":
         if computer == "scissors":
             print("you lose! but u got it next time!!!!")
+            print("#__________#")
             playerlives = playerlives - 1
         else:
             print("you win!    !!cheers!! :)")
+            print("#__________#")
             computerlives = computerlives - 1
 
     elif player == "scissors":
         if computer == "rock":
             print("you lose! but u got it next time!!!!")
+            print("#_________#")
             playerlives = playerlives - 1
         else:
             print("you win!    !!cheers!! :)")
+            print("#________#")
             computerlives = computerlives - 1
 
     #returning the value to main_game
--- gamecomponents/winlose.py
+++ gamecomponents/winlose.py
@@ -1,22 +1,30 @@
 
 #defining function winorlose to decide whether to play again or not
 def winorlose(status, player, playerlives, computerlives):     
-                      
+
         print("You " + status + "! Would u like to play again?   ")
-        Choice = input("Y / N")       #user inputs
+        Choice = input("Y / N: ")       #user inputs
 
         if Choice == "n" or Choice == "N":
+            print("you chose NO!!!")
             print ("Better luck next time and see you again!!!")
+            print("================")
             exit()
             #user input "n", so exiting from the loop (game)
 
         elif Choice == "y" or Choice == "Y":
-            print("good to gooo....")
-            #reset and restart game
-
+            print("you chose YES!!! u r good to gooo!!!!!!!!!!!!!....")
             playerlives = 2
             computerlives = 2
             player = False    
+            
+            print("***")
+            print("**")
+            print("*")
+            print("**")
+            print("***")
+            
+            #reset and restart game
 
             #returning values to main_game
             return (playerlives, computerlives, player)
