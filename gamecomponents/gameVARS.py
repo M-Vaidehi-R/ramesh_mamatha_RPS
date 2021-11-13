4c26fff71c46fbd797f2422e7a62b3a9de599152
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2021-11-12 23:07:16 -0500

    working main file and functions

---
+++ gamecomponents/gameVARS.py
@@ -0,0 +1,11 @@
+from random import randint
+player = False
+
+
+choices = ["rock", "paper", "scissors"]
+
+#random function to choose a weapon for computer
+computer = choices[randint(0, 2)]
+
+playerlives =1 
+computerlives =1
